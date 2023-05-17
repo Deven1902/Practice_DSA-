7404971d9711fdb57ae8f73c3bfaed3eac24ef19
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-05-17 15:50:46 +0530

    Recursion Revision

--- recursion.cpp
+++ recursion.cpp
@@ -1,83 +1,131 @@
-// recurssion
+// what is recursion!?  
 
-/* Direct recursion:- function is called within itself.
 
-indirect recurssion:- 2 functions are calling each other. 
+// What are the applications of Recursion:- 
 
-eg main()  {
-fun()1;
+/*
+Recursion can be interchangebly used with iteration. What ever can be done with iteration that same can be donw with the recursion as well. 
+vice a versa. 
+
+Recursive solutions make things simpler on their iterative counterparts. 
+
+Many common algos based on recursion:- Dynamic Programing, Backtracking, Divide and Conquer. 
+
+Common problems:- Tower of hanoi, DFS based traversal. 
+
+*/
+
+// example code 1:- 
+
+ /* #include <iostream>
+using namespace std;
+
+
+void fun(int n)
+{
+	if(n == 0)
+		return;
+
+	cout<<n<<endl;
+
+	fun(n - 1);
+	
+	cout<<n<<endl;
 }
+int main() {
+	
+	fun(3);
+	
+	return 0;
+} 
 
-void fun1() {
-fun2();
-}
-
-each process will get 2mb stack allocation. 
-beyonf 2mb if a provess is trying to allocate stack sace. then error
-stack overflow will occur.
-this will give error but... stack overflow and segmentation fault. 
-
-solution: identify the ase case appropriately, which will termiante the function call
-and returns the execution control.
-
-you should pass a paramter, vaklue of that paramter should keep changing 
-and check the valye of that oaramter and terminate/ return from function based on condition. 
-
-
+Output looks like:- 3
+2
+2
+1
+2
+3
 */ 
 
-// indirect recursion example recursion. 
+// example 2:- 
+/* 
+#include <iostream>
+using namespace std;
 
-/*#include<iostream>
-using namespace std; 
 
-void fun2(int j) {
-	void fun1(int i); 
-	
-	fun1(j+1);
-	cout<<"fun2 ends"<<endl; 
-}
-
-void fun1(int i) {
-	if(i>5)
+void fun(int n)
+{
+	if(n == 0)
 		return;
-	fun2(i+1);
-	cout<<"Fun1 ends"<<endl; 
-}
 
-int main() {
-	fun1(5);
-	cout<<"Recurssion ends"<<endl; 
-	return 0;  
+	fun(n - 1);
+	
+	cout<<n<<endl;
+	
+	fun(n - 1);
 }
+int main() {
+	
+	fun(3);
+	
+	return 0;
+} 
+
+Output:- Looks like:- 1
+2
+1
+3
+1
+2
+1
 */
 
-// winding and un winding of function 
-
-/* forward recursive statements :- which get excuted while winfing of function calls.
-
-backward                       :- which get excuted while unwinding of function calls.
-
-
-*/
-
-#include<iostream>
-using namespace std; 
+// example:- Recursive program dispaying the nature of log n:- 
+/* 
+#include <iostream>
+using namespace std;
 
 
-
+int fun(int n)
+{
+	if(n == 1)
+		return 0;
+	else
+		return 1 + fun(n / 2);
+}
 int main() {
-	void fun1(int n) {
-		if(n>5) return;
-		
-		cout<<"\t"<<n;  // forward recursive statement. 
-		fun1(n+1);
-		//cout<<endl;    // this is backward statement. 
-		//cout<<"\t"<<n;
-		return; 
-	}
-	return 0; 
+	
+	cout<<fun(1);
+	
+	return 0;
+} 
+
+Output looks like:- 4 
+this shows the conversion of a number to its log counter part recursively. 
+*/ 
+
+// example showing binary conversion of number using recursive calls:- 
+/*
+#include <iostream>
+using namespace std;
+
+// recursive function to change the number to binary. 
+void fun(int n)
+{
+	if(n == 0)
+		return;
+	
+	fun(n / 2);
+
+	cout<<(n % 2)<<endl;
+}
+int main() {
+	
+	fun(7);
+	
+	return 0;
 }
 
-
-
+output:- 111
+ 
+*/
