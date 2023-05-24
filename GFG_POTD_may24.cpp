d5dfde26c982109d4ac47c7023825af8592954c5
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-05-24 08:58:43 +0530

    Update GFG_POTD_may24.cpp

--- GFG_POTD_may24.cpp
+++ GFG_POTD_may24.cpp
@@ -1,3 +1,21 @@
+/* 
+Author:- Deven Nandapurkar 
+
+Date:- 24th may
+
+Problem Link:- https://practice.geeksforgeeks.org/problems/5bfe93cc7f5a214bc6342709c78bc3dceba0f1c1/1
+
+Problem:- There are N bowls containing cookies. In one operation, you can take one cookie from any of the non-empty bowls and put it into another bowl. 
+          If the bowl becomes empty you discard it. You can perform the above operation any number of times. 
+          You want to know the maximum number of bowls you can have with an identical number of cookies.
+
+Note: All the non-discarded bowls should contain the identical number of cookies.
+
+Difficulty:- Easy
+
+Cpp solution below 👇👇
+*/
+
 class Solution {
   public:
     int getMaximum(int N, vector<int> &arr) {
