ee520cfb6fd3c10cd483089597ce9ac1cbae5a4b
Deven1902 <72366367+Deven1902@users.noreply.github.com>
2023-06-03 10:45:03 +0530

    alterbnate solution

--- GFG_POTD_June3.cpp
+++ GFG_POTD_June3.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution{
 public:
     int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
@@ -40,6 +41,66 @@ public:
                 z -= S3[k++];
         }
         
+        return 0;
+    }
+};
+*/ 
+
+// alternate solution:- 
+
+class Solution{
+public:
+    int maxEqualSum(int N1,int N2,int N3,vector<int> &S1,vector<int> &S2,vector<int> &S3){
+    // code here
+        // elements at the top of the stack
+        int i=0, j=0, k=0; 
+        
+        // indicates sum of the stack. 
+        int sum1=0,sum2=0,sum3=0;
+        
+        //  accumulate(): 
+        // This function returns the sum of all the values lying in a range between [first, last] with the variable sum.
+        
+        sum1=accumulate(S1.begin(),S1.end(),sum1);
+        sum2=accumulate(S2.begin(),S2.end(),sum2);
+        sum3=accumulate(S3.begin(),S3.end(),sum3);
+        
+        while(i<N1 and j<N2 and k<N3) { // loop will run untill anyone of the stacks get empty. 
+        
+            if(sum1==sum2 and sum2==sum3)
+                return sum1;
+                
+            else if(sum1<=sum2 and sum1<=sum3) {
+                if(sum1<sum2) {
+                    sum2-=S2[j];
+                    j++;
+                }
+                if(sum1<sum3){
+                    sum3-=S3[k];
+                    k++;
+                }
+            }
+            else if(sum2<=sum1 and sum2<=sum3){
+                if(sum2<sum1){
+                    sum1-=S1[i];
+                    i++;
+                }
+                if(sum2<sum3){
+                    sum3-=S3[k];
+                    k++;
+                }
+            }
+            else{
+                if(sum3<sum2){
+                    sum2-=S2[j];
+                    j++;
+                }
+                if(sum3<sum1){
+                    sum1-=S1[i];
+                    i++;
+                }
+            }
+        }
         return 0;
     }
 };
