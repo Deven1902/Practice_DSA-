4fa6d3006f9cd1eb50b52ce17fd7bf15d3ac52c5
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-06-24 18:45:56 +0530

    Update optimised solution

--- Medium/Prefix match with other strings/prefix-match-with-other-strings.cpp
+++ Medium/Prefix match with other strings/prefix-match-with-other-strings.cpp
@@ -1,14 +1,8 @@
-//{ Driver Code Starts
-//Initial template for C++
-
-#include<bits/stdc++.h> 
-using namespace std; 
-
-// } Driver Code Ends
-//User function Template for C++
-
 class Solution{   
 public:
+
+// Brute Force Solution
+    /*
     int klengthpref(string a[], int n, int k, string s){    
         if(s.size()<k)
             return 0;
@@ -25,33 +19,50 @@ public:
         }
         return cnt;
     }
-};
-
-//{ Driver Code Starts.
-int main() 
-{ 
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n;
-        cin>>n;
-        string arr[n];
-        for(int i=0;i<n;i++)
-        {
-            string s;
-            cin>>arr[i];
-            
-        }
-        int k;
-        cin>>k;
-        string str;
-        cin>>str;
+    */
+    
+    // time complexity:- O(n*k)
+    // space complexity:- O(n) 
+    
+    // optimised solution 👇👇
+    
+    struct Node {
+        int pre_cnt;
+        unordered_map<char,Node*>next;
         
-        Solution ob;
-        cout << ob.klengthpref(arr, n, k, str) << endl;
+        Node() {
+            pre_cnt=0;
+        }
+    };
+    
+    int klengthpref(string a[], int n, int k, string s) {    
+        if(s.length()<k)
+            return 0;
+            
+        Node* root = new Node();
+        int i,j,cnt=0;
+        
+        for(i=0;i<n;i++) {
+            string x=a[i];
+            Node *temp=root;
+            for(j=0;j<x.length();j++) {
+                if(temp->next.find(x[j])==temp->next.end())temp->next[x[j]]=new Node();
+                temp=temp->next[x[j]];
+                temp->pre_cnt++;
+            }
+        }
+        
+        Node *temp=root;
+        for(i=0;i<k;i++) {
+            if(temp->next.find(s[i])==temp->next.end())return 0;
+            else temp=temp->next[s[i]];
+        }
+        
+        return temp->pre_cnt;
     }
-    return 0; 
-} 
-
-// } Driver Code Ends
+    
+    // Time Complexity: O(n*l)
+    // Space Complexity: O(m*l)
+    
+    // m-> no of unique characters present in strings
+    // l -> length of longest string present in the array
