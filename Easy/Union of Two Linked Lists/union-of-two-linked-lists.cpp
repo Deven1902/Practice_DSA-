a751cb1333e67f21b9ee4603946edb75e221a049
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-06-28 07:17:53 +0530

    update details of the problem

--- Easy/Union of Two Linked Lists/union-of-two-linked-lists.cpp
+++ Easy/Union of Two Linked Lists/union-of-two-linked-lists.cpp
@@ -1,3 +1,34 @@
+// Author:- Deven Nandapurkar
+/*
+Problem:- Union of Two Linked Lists. 
+
+Given two linked lists, your task is to complete the function makeUnion(), that returns the union list of two linked lists. 
+This union list should include all the distinct elements only and it should be sorted in ascending order.
+
+Problem link:- https://practice.geeksforgeeks.org/problems/union-of-two-linked-list/1 
+
+Date:- 27th June.
+
+Difficulty:- Easy.
+
+Topic:- Sorting/ linkedList
+
+Cpp solution 👇👇
+*/
+
+// Thought process-> First combine the 2 linkedList and then sort them in asceding order. 
+
+// ptoblme is a slight modification of merge sort algo. 
+    // Step 1: - Merge the given two lists.
+    
+    // Step 2: - Sort the merged List
+    
+    // Step 3: - Remove Duplicate elements from the final List.
+
+// we will be using merge sort here... due to following reasons. 
+    // 1. Merge Sort Technique uses a merge function to combine lists. So we don't need to write an explicit function for STEP 1.
+    // 2. While merging the Elements of two lists, we can handle duplicate elements by adding a simple condition.
+
 class Solution
 {
     
