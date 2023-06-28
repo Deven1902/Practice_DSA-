b59d4b450e845fe96070bc519402db6ce38e75a3
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-06-28 07:12:10 +0530

    refactor code

--- Easy/Union of Two Linked Lists/union-of-two-linked-lists.cpp
+++ Easy/Union of Two Linked Lists/union-of-two-linked-lists.cpp
@@ -1,48 +1,5 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-struct Node
-{
-	int data;
-	struct Node* next;
-	
-	Node(int x){
-	    data = x;
-	    next = NULL;
-	}
-	
-};
-
-
-// } Driver Code Ends
-/*
-// structure of the node is as follows
-
-struct Node
-{
-	int data;
-	struct Node* next;
-	
-	Node(int x){
-	    data = x;
-	    next = NULL;
-	}
-	
-};
-
-*/
 class Solution
 {
-    public:
-    struct Node* makeUnion(struct Node* head1, struct Node* head2)
-    {
-        // code here
-        
-        Node* merged = merge(head1, head2);
-        Node* sorted = sort(merged);
-        return sorted;
-    }
     
     private:
     Node* merge(Node* a, Node* b) {
@@ -53,13 +10,17 @@ class Solution
 
         Node* ans = nullptr;
 
-        if (a->data < b->data) {
+        if(a->data < b->data) {
             ans = a;
             ans->next = merge(a->next, b);
-        } else if (b->data < a->data) {
+        } 
+        
+        else if(b->data < a->data) {
             ans = b;
             ans->next = merge(a, b->next);
-        } else {
+        } 
+        
+        else {
             ans = a;
             ans->next = merge(a->next, b->next);
         }
@@ -90,58 +51,16 @@ class Solution
 
         return ans;
     }
+    
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        // code here
+        
+        Node* merged = merge(head1, head2);
+        Node* sorted = sort(merged);
+        return sorted;
+    }
+// time complexity:- O(n log n).
+// space complexity:- O(n).
 };
-
-
-//{ Driver Code Starts.
-
-struct Node* buildList(int size)
-{
-    int val;
-    cin>> val;
-    
-    Node* head = new Node(val);
-    Node* tail = head;
-    
-    for(int i=0; i<size-1; i++)
-    {
-        cin>> val;
-        tail->next = new Node(val);
-        tail = tail->next;
-    }
-    
-    return head;
-}
-
-void printList(Node* n)
-{
-    while(n)
-    {
-        cout<< n->data << " ";
-        n = n->next;
-    }
-    cout<< endl;
-}
-
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n, m;
-        
-        cin>>n;
-        Node* first = buildList(n);
-        
-        cin>>m;
-        Node* second = buildList(m);
-        Solution obj;
-        Node* head = obj.makeUnion(first,second);
-        printList(head);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
