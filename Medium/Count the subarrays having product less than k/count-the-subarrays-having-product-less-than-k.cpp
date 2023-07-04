3c247b00e03e3814ebbfae90eb19f482a8758834
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-07-04 18:48:18 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count the subarrays having product less than k/count-the-subarrays-having-product-less-than-k.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+// } Driver Code Ends
+
+
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        // this is a sliding window approach! 
+        
+        int cnt = 0;
+        long long prod=1;
+        int l=0, r=0; // left and right pointers 
+        
+        while(r<n) {
+            prod*=a[r];
+            
+            while(prod>=k && l<=r) {
+                prod = prod/a[l];
+                l++;
+            }
+            
+            cnt+=r-l+1;
+            r++;
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        long long int k;
+        cin >> n >> k;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) cin >> arr[i];
+        Solution obj;
+        cout << obj.countSubArrayProductLessThanK(arr, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
