f04bbefbbcef53f79fe942b28ca2b383db08d50c
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-07-13 12:42:06 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Number of Occurrences/unique-number-of-occurrences.py
@@ -0,0 +1,53 @@
+
+from typing import List
+
+
+class Solution:
+    def isFrequencyUnique(self, n : int, arr : List[int]) -> bool:
+        # code here
+        
+        l = list(set(arr))
+        f=[]
+        
+        for x in l:
+            if arr.count(x) in f:
+                return False
+            
+            f.append(arr.count(x))
+            
+        return True
+        
+
+
+
+#{ 
+ # Driver Code Starts
+
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.isFrequencyUnique(n, arr)
+        
+        result_val = 1 if res else 0
+        print(result_val)
+
+# } Driver Code Ends
