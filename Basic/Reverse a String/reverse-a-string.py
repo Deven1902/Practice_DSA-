27813449f685ea5a73ba611994e0ef2f4925b76f
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-08-27 08:58:56 +0530

    Updated Solution - GfG to GitHub

--- Basic/Reverse a String/reverse-a-string.py
+++ Basic/Reverse a String/reverse-a-string.py
@@ -1,11 +1,18 @@
 #User function Template for python3
 
-def reverseWord(s):
-    #your code here
-    
-    return s[::-1]
-    x = reverseWord("Geeks")
-    print(x)
+'''
+Extended slice operator:- puts “step” fields as [start, stop, step].
+                        giving no field as start and stop indicates default to 0 and string length respectively.
+                        -1 denotes starting from the end and stop at the start, hence reversing a string. 
+'''
+
+# simple one line python code
+
+class Solution:
+    def reverseWord(self, s):
+        #your code here
+        
+        return s[::-1]
 
 
 #{ 
@@ -16,7 +23,8 @@ if __name__ == "__main__":
     t = int(input())
     while(t>0):
         s = input()
-        print(reverseWord(s))
+        ob = Solution()
+        print(ob.reverseWord(s))
         t = t-1
 
 # } Driver Code Ends
