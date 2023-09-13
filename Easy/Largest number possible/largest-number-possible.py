10164e9bc8f3e21c5c6876cf5f5c609191a15d5b
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-13 09:33:25 +0530

    refactoring

--- Easy/Largest number possible/largest-number-possible.py
+++ Easy/Largest number possible/largest-number-possible.py
@@ -10,11 +10,14 @@ class Solution:
         if (N>1 and S==0) or (N*9 < S):
             return -1
         
+        
+        '''
         # start from the leftmost digit... and fill it with the largest possible digit (9 obvio). 
         # do this as long as the sum is <= S.  
         # then move to the next digit. 
         
         # we repeat this untile we have made the N digit number. 
+        '''
         
         largestN=""
         for digit_position in range(N):
