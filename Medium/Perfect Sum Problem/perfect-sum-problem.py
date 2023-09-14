0c87080a0f765d695226dbe0b70fc3250fd1166f
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-14 08:11:16 +0530

    update approach

--- Medium/Perfect Sum Problem/perfect-sum-problem.py
+++ Medium/Perfect Sum Problem/perfect-sum-problem.py
@@ -1,4 +1,17 @@
 #User function Template for python3
+
+# apporach :- DP 
+
+'''
+    1. Initialize a dynamic programming array, dp, with 0-based indexing.
+    2. Set dp[0] to 1 because there is one way to achieve a sum of 0, which is by choosing an empty subset.
+    3. Iterate through the elements in the input array numbers.
+    4. For each element number, iterate through the possible sums in reverse order, from targetSum down to number.
+    5. Calculate the count of perfect sums for the current sum by adding the count of perfect sums for (currentSum - number). Update dp[currentSum] accordingly.
+    6. Finally, return dp[targetSum] as the count of perfect sums for the target sum, modulo 10^9 + 7.
+'''
+
+
 class Solution:
 	def perfectSum(self, arr, n, sum):
 		# code here
