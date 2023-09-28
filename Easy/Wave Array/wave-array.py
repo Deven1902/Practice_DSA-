3bc6646334fc3b1038ea10856d10be2c5358efcc
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-09-28 09:18:06 +0530

    updates complexity analysis

--- Easy/Wave Array/wave-array.py
+++ Easy/Wave Array/wave-array.py
@@ -16,31 +16,6 @@ class Solution:
                 a[i+1]=temp
         return a
 
-#{ 
- # Driver Code Starts
-class IntArray:
-    def __init__(self) -> None:
-        pass
-    def Input(self,n):
-        arr=[int(i) for i in input().strip().split()]#array input
-        return arr
-    def Print(self,arr):
-        for i in arr:
-            print(i,end=" ")
-        print()
+# time complexity:- O(n). 
 
-
-if __name__=="__main__":
-    t = int(input())
-    for _ in range(t):
-        
-        n = int(input())
-        
-        
-        a=IntArray().Input(n)
-        
-        obj = Solution()
-        obj.convertToWave(n, a)
-        IntArray().Print(a)
-
-# } Driver Code Ends
+# space complexity:- O(1). 
