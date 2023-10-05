e90ba3bd2f8dbc996af57ae2e4fafdbb296fb8a4
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-05 10:00:33 +0530

    Update approach

--- Medium/Count number of substrings/count-number-of-substrings.py
+++ Medium/Count number of substrings/count-number-of-substrings.py
@@ -1,5 +1,22 @@
 #User function Template for python3
 
+'''
+Approach
+
+We'll use a sliding window approach and a dictionary to track character counts within the window. Here's the step-by-step approach:
+
+1. Initialize a counter for the substrings (count), an empty current substring (current_substring), a dictionary to track character counts (character_count), and the starting index of the current substring (start_index).
+
+2. Iterate through the input string, adding characters to current_substring and updating character_count accordingly.
+
+3. While the count of distinct characters in character_count exceeds k, remove characters from the start of the substring and update character_count. This step ensures that we maintain exactly k distinct characters within the current window.
+
+4. Increment count by the length of the current substring (end_index - start_index + 1) at each iteration. This accounts for all valid substrings with exactly k distinct characters ending at the current index.
+
+5. Return the count as the final result.
+
+'''
+
 class Solution:
     def count_substrings_with_k_distinct_characters(self, s, k):
         count = 0  # Initialize a counter for the substrings
