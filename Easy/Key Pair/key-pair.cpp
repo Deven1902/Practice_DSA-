249a116750e5133bc2eeba47fe77967b7a395f0b
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2023-10-13 10:23:18 +0530

    added approach

--- Easy/Key Pair/key-pair.cpp
+++ Easy/Key Pair/key-pair.cpp
@@ -11,26 +11,31 @@ public:
 	// Function to check if array has 2 elements
 	// whose sum is equal to the given value
 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
-	    // code here
-	    
-	    sort(arr, arr+n);
-	    int sum = 0;
-	    int i=0, j=n-1;
-	    
-	    while(i<j) {
-	        sum = arr[i] + arr[j];
-	        
-	        if(sum == x)
-	            return true;
-	            
-	        else if(sum > x)
-	            j--;
-	            
-	        else
-	            i++;
-	    }
-	    return false;
-	}
+        // Sort the array in ascending order
+        sort(arr, arr + n);
+    
+        // Initialize variables for the sum and two pointers
+        int sum = 0;
+        int i = 0, j = n - 1;
+    
+        // Loop until the pointers meet
+        while (i < j) {
+            // Calculate the sum of the elements at the current positions
+            sum = arr[i] + arr[j];
+    
+            // If the sum is equal to the target value, return true
+            if (sum == x)
+                return true;
+            // If the sum is greater than the target value, decrement the right pointer
+            else if (sum > x)
+                j--;
+            // If the sum is less than the target value, increment the left pointer
+            else
+                i++;
+        }
+    
+        // If no pair is found, return false
+        return false;
 };
 
 //{ Driver Code Starts.
