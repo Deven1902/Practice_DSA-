bdd15b2732a693a07045e12cd9692b04ee497ed8
Deven Nandapurkar <72366367+Deven1902@users.noreply.github.com>
2025-01-05 22:06:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Pairs whose sum is less than target/count-pairs-whose-sum-is-less-than-target.py
@@ -0,0 +1,49 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+# } Driver Code Ends
+#User function Template for python3
+class Solution:
+    #Complete the below function
+    def countPairs(self, arr, target):
+        #Your code here
+        
+        arr.sort() 
+        i=0
+        cnt = 0
+        j = len(arr)-1
+        
+        while i<j: 
+            while j>i and arr[i]+arr[j]>=target:
+                j-=1
+                
+            cnt +=j-i
+            i+=1
+            
+        return cnt
+        
+
+#{ 
+ # Driver Code Starts.
+
+def main():
+    T = int(input())
+    while (T > 0):
+
+        A = [int(x) for x in input().strip().split()]
+
+        k = int(input())
+        ob = Solution()
+        print(ob.countPairs(A, k))
+        print('~')
+        T -= 1
+
+
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
